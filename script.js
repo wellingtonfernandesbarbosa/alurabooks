be0eb5c9a4331001b994e82c5d9bb2d66d37e72f
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-06-23 00:04:27 -0300

    Recuperando dados de endereço por API

---
+++ script.js
@@ -0,0 +1,32 @@
+async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML  = "";
+
+    try {
+        var consultaCEP = await fetch(`http://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+
+        if (consultaCEPConvertida.erro){
+            throw Error('CEP não existe!');
+        }
+
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var estado = document.getElementById('estado');
+        var bairro = document.getElementById('bairro');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        return consultaCEPConvertida;
+    } catch (erro) {
+        mensagemErro.innerHTML = `<p>Esse CEP pertence a uma galáxia muito, muito distante. Infelizmente, nossos livros não podem viajar tão longe.</p>`;
+        console.log(erro);
+    };
+};
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+ 
