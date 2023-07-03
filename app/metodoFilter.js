0c2c80530bbc88dd9acb3fd3f3a73fa6bdd88155
Wellington Fernandes <wellsfernandes1991@gmail.com>
2023-07-03 10:15:49 -0300

    Filtros por preço e disponibilidade
    Implementada funcionalidade de filtragem e ordenação na página de categorias, permitindo ao usuário ordenar os livros por preço e filtrar por disponibilidade.

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -5,6 +5,14 @@ botoes.forEach(btn => btn.addEventListener('click', filtrarLivros));
 function filtrarLivros() {
     const elementoBtn = document.getElementById(this.id);
     const categoria = elementoBtn.value;
-    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria);
+    let livrosFiltrados = categoria == 'disponivel' ? filtrarPorDisponibilidade() : filtrarPorCategoria(categoria);
     exibirOsLivrosNaTela(livrosFiltrados);
+};
+
+function filtrarPorCategoria(categoria) {
+    return livros.filter(livro => livro.categoria == categoria);
+};
+
+function filtrarPorDisponibilidade() {
+    return livros.filter(livro => livro.quantidade > 0);
 };
